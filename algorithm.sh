ab87516c71998c50d823246ca9c049f66e227fe6
Sergey Ryakhovsky <66959150+rsergo97@users.noreply.github.com>
2022-09-20 00:34:26 +0300

    Add files via upload

---
+++ algorithm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+if [ "$#" -eq 0 ] || [ $1 = "-h" ]; then \
+  echo "Usage: bash algorithm.sh normal.bam tumor.bam sample_ID hg19/hg38 outdir workdir"; exit
+fi
+
+
+# Arguments 
+normal_bam=$1
+tumor_bam=$2
+sample_name=$3
+hg=$4
+outdir=$5
+workdir=$6
+
+echo "Start ..."
+
+cd ${outdir}
+
+samtools mpileup ${normal_bam} -o normal.mpileup
+
+samtools mpileup ${tumor_bam} -o tumor.mpileup 
+
+varscan somatic normal.mpileup tumor.mpileup ${sample_name}.varscan
+
+echo "Pileup has beed calculated"
+
+python ${workdir}/process_pileup.py -i ${sample_name}.varscan.snp -o ${sample_name}.to_call.txt 
+
+echo "CNA calling"
+# CNA calling by Facets script
+
+Rscript ${workdir}/facets_call.R ${sample_name}.to_call.txt ${hg} ${outdir}
+
+echo "Done! Segments and CNA plot are ready."
+
+
